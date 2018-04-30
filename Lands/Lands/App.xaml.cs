4b8857ad036c380e1113fa756e738976e2950351
Neftalí Vicente Arroyo <neftavicente@hotmail.com>
2018-04-30 08:56:50 +0200

    Hasta Video5

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Lands.Views;
+
+using Xamarin.Forms;
+
+namespace Lands
+{
+	public partial class App : Application
+	{
+        #region Constructors    
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage(new LoginPage()); // NavigationPage para navegar entre paginas (rayita arriba donde va el título)
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,23 @@
+﻿using Lands.ViewModels;
+
+namespace Lands.Infrastructure 
+{
+    // Relaciona la página LoginPage con la MainViewModel
+    public class InstanceLocator
+    {
+        #region Properties 
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion        
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
